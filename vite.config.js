f048089eadc631657df4df1731fb03eeb6b93180
ben.sanchez <ben.sanchez@mastercard.com>
2025-12-07 20:04:21 -0500

    Move files around

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
-    include: ["pub/**/**test.mjs", "pub/**/**test.jsx"],
+    include: ["tests/**/**test.mjs", "tests/**/**test.jsx"],
   },
 });
 
